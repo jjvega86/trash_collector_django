490daef35ee060c0df8bcfd0880e2bcf934c3147
JJ <jj.vega86@gmail.com>
2021-04-13 15:25:23 -0400

    Finished adding Customer, Address, Account models and properties in customers/models.py

--- customers/models.py
+++ customers/models.py
@@ -1,21 +1,25 @@
 from django.db import models
-# Create your models here.
-
-# TODO: Finish customer model by adding necessary properties to fulfill user stories
 
 
 class Customer(models.Model):
     PICKUP_DAYS = (
         ('Mo', 'Monday'),
         ('Tu', 'Tuesday'),
-        ('Mo', 'Wednesday'),
-        ('Mo', 'Thursday'),
-        ('Mo', 'Friday'),
+        ('We', 'Wednesday'),
+        ('Th', 'Thursday'),
+        ('Fr', 'Friday'),
     )
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
     address = models.ForeignKey('customers.Address', default=0, on_delete=models.CASCADE)
-    weekly_pickup = models.CharField(max_length=2)
+    account = models.ForeignKey('customers.Account', default=0, on_delete=models.CASCADE)
+    weekly_pickup = models.CharField(max_length=2, choices=PICKUP_DAYS)
+    extra_pickup = models.DateField()
+    suspension_start = models.DateField()
+    suspension_end = models.DateField()
+
+    def __str__(self):
+        return self.name
 
 
 class Address(models.Model):
@@ -26,3 +30,6 @@ class Address(models.Model):
     zipcode = models.IntegerField(max_length=10)
 
 
+class Account(models.Model):
+    account_number = models.IntegerField()
+    account_balance = models.DecimalField(max_digits=10, decimal_places=2)
