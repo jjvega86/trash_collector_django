e9628df671a614b0a2ab27d5cc923b0be4b1c679
JJ <jj.vega86@gmail.com>
2021-04-13 15:48:43 -0400

    Fixed ForeignKey issues on Customer model; nuked db + migrations, reinstalled dependencies, remigrated. Models complete

--- customers/models.py
+++ customers/models.py
@@ -10,26 +10,27 @@ class Customer(models.Model):
         ('Fr', 'Friday'),
     )
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
-    address = models.ForeignKey('customers.Address', default=0, on_delete=models.CASCADE)
-    account = models.ForeignKey('customers.Account', default=0, on_delete=models.CASCADE)
-    weekly_pickup = models.CharField(max_length=2, choices=PICKUP_DAYS)
-    extra_pickup = models.DateField()
-    suspension_start = models.DateField()
-    suspension_end = models.DateField()
+    user = models.ForeignKey('accounts.User', default=None, on_delete=models.CASCADE)
+    address = models.ForeignKey('customers.Address', default=None, null=True, on_delete=models.CASCADE)
+    account = models.ForeignKey('customers.Account', default=None, null=True, on_delete=models.CASCADE)
+    weekly_pickup = models.CharField(max_length=2, choices=PICKUP_DAYS, null=True, default='Mo')
+    extra_pickup = models.DateField(null=True)
+    suspension_start = models.DateField(null=True)
+    suspension_end = models.DateField(null=True)
 
     def __str__(self):
         return self.name
 
 
 class Address(models.Model):
-    building_number = models.IntegerField(max_length=10)
+    building_number = models.IntegerField()
     street_name = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
-    zipcode = models.IntegerField(max_length=10)
+    zipcode = models.IntegerField()
 
 
 class Account(models.Model):
     account_number = models.IntegerField()
     account_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    last_charge = models.DateField(null=True)
--- employees/models.py
+++ employees/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
-    zipcode = models.IntegerField(max_length=10)
+    zipcode = models.IntegerField()
